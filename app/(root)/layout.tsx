3a227b02eeef5fd9a81e211f24fccc0263cbe198
HumaneLogic <forgodforlogic@gmail.com>
2025-01-10 17:35:46 +0330

    add mobilenav ,  chart

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import Image from 'next/image';
+import MobileNav from '../../components/MobileNav';
+import SideBar from '../../components/SideBar';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'amir', LastName: 'JSM' };
   return (
-    <main>
-      SIDEBAR
+    <main className='flex h-screen w-full font-inter'>
+      <SideBar user={loggedIn} />
+
+      <div className='flex size-full flex-col'>
+        <div className='root-layout'>
+          <Image src='/icons/logo.svg' width={30} height={30} alt='logo' />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
       {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import HeaderBox from '../../components/HeaderBox';
+import TotalBalanceBox from '../../components/TotalBalanceBox';
 
- const Home = () => {
+const Home = () => {
+  const loggedIn = { firstName: 'amir' };
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            title='welcome'
+            type='greeting'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions efficiently'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+export default Home;
